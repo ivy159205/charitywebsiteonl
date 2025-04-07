67d2947bd16efab3ce608e321415bed8bfe81835
ivy159205 <haiduc159205.hsr@outlook.com>
2025-04-07 14:02:30 +0700

    fix lỗi tồn đọng

--- FINAL_Website_Code/Charity_Website_API_and_Frontend/charifit-master-updated_by_huy/charifit-master/js/login.js
+++ FINAL_Website_Code/Charity_Website_API_and_Frontend/charifit-master-updated_by_huy/charifit-master/js/login.js
@@ -36,3 +36,24 @@ document.getElementById("loginForm").addEventListener("submit", async function (
         document.getElementById("message").innerText = "Không thể kết nối tới server!";
     }
 });
+
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
+}
+
+function remcl() {
+    let parent = this.parentNode.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+
+inputs.forEach(input => {
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
+});
